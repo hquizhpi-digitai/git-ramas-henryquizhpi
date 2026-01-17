4a1eaa72d0f4ba5df0f4ed85ae14ef3493e80d00
hquizhpi-digitai <hquizhpi@digitai.tech>
2026-01-17 11:51:01 -0500

    feat: agregar main.js
    Archivo JavaScript principal con funciones de inicialización del proyecto

---
+++ main.js
@@ -0,0 +1,23 @@
+// Taller GitHub Maestria grupo4:
+// Integrantes: Steven Herrera
+//              Henry Quizhpi
+//              Nicolas Carrasco
+//              Nicolas Alvarez
+//              Brayan Taday
+// Rama: main
+
+console.log('Iniciando en rama main');
+
+function saludar() {
+    return 'Hola !! desde la rama principal';
+}
+
+function iniciarProyecto() {
+    console.log('Bienvenido al proyecto de Git con ramas');
+    console.log('Grupo 4');
+}
+
+// Inicializar
+iniciarProyecto();
+
+   feat: agregar main.js
